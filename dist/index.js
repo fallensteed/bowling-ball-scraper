3cc1818839db21e8fbc09257c6d9bc7047a35301
Scott Hubert <scott@hubertart.com>
2022-07-24 21:54:24 -0600

    Started looking at secondary pages for details

--- dist/index.js
+++ dist/index.js
@@ -9,6 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 import fetch from "node-fetch";
 import * as cheerio from "cheerio";
+import pretty from "pretty";
 const getRawData = (url) => {
     return fetch(url)
         .then((response) => response.text())
@@ -16,11 +17,45 @@ const getRawData = (url) => {
         return data;
     });
 };
-const url = "https://www.stormbowling.com/storm-balls";
+const stormURL = "https://www.stormbowling.com/storm-balls";
+const ballList = [];
 const getStormBallList = () => __awaiter(void 0, void 0, void 0, function* () {
-    const stormBallListRawData = yield getRawData(url);
+    const stormBallListRawData = yield getRawData(stormURL);
     const $ = cheerio.load(stormBallListRawData);
-    const productTiles = $("#ProductTiles .product-tile .title");
-    console.log(productTiles.text());
+    const productTiles = $(".product-tile");
+    const stormBallList = [];
+    productTiles.each((i, el) => {
+        const ballName = pretty($(".title", el).text());
+        if (ballName)
+            stormBallList.push({ name: ballName, companyName: "Storm" });
+    });
+    productTiles.each((i, el) => {
+        const ballUrl = $("a", el).prop("href");
+        stormBallList[i].url = stormURL + ballUrl;
+    });
+    // stormBallList.forEach(async (ball) => {
+    //     if (ball.url) {
+    //         const stormBallDetailRawData = await getRawData(ball.url)
+    //         const $ = cheerio.load(stormBallDetailRawData)
+    //         const span = $("p")
+    //         span.each((i, el) => {
+    //             if ($("p", el).text().includes("Release Date")) console.log( $("p", el).text())
+    //         })
+    //     }
+    // })
+    stormBallList.forEach((ball) => ballList.push(ball));
+    getBallDetailsTestFunction(ballList[0]);
+});
+const getBallDetailsTestFunction = (ball) => __awaiter(void 0, void 0, void 0, function* () {
+    if (ball.url) {
+        const stormBallDetailRawData = yield getRawData(ball.url);
+        const $ = cheerio.load(stormBallDetailRawData);
+        const span = $(".product-detail > .row-fluid > .span4");
+        console.log(span.html());
+        // span.each((i, el) => {
+        // 	if ($("p", el).text().includes("Release Date"))
+        // 		console.log($("p", el).text());
+        // });
+    }
 });
 getStormBallList();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import fetch from "node-fetch";
 import * as cheerio from "cheerio";
+import pretty from "pretty";
 
 const getRawData = (url: string) => {
 	return fetch(url)
@@ -9,13 +10,71 @@ const getRawData = (url: string) => {
 		});
 };
 
-const url = "https://www.stormbowling.com/storm-balls";
+const stormURL = "https://www.stormbowling.com/storm-balls";
+
+type CoreType = "symmetrical" | "asymmetrical";
+type CoverType =
+	| "urethane solid"
+	| "urethane pearl"
+	| "solid reactive"
+	| "hybrid reactive"
+	| "pearl reactive";
+interface BallModel {
+	name: string;
+	companyName: string;
+	url?: string;
+	companyBallId?: string;
+	imageUrl?: string;
+	releaseDate?: Date;
+	factoryFinish?: string;
+	coreType?: CoreType;
+	coreName?: string;
+	coverType?: CoverType;
+	coverName?: string;
+}
+
+const ballList: BallModel[] = [];
 
 const getStormBallList = async () => {
-	const stormBallListRawData = await getRawData(url);
-	const $ = cheerio.load(stormBallListRawData)
-    const productTiles = $("#ProductTiles .product-tile .title")
-    console.log(productTiles.text())
+	const stormBallListRawData = await getRawData(stormURL);
+	const $ = cheerio.load(stormBallListRawData);
+	const productTiles = $(".product-tile");
+	const stormBallList: BallModel[] = [];
+	productTiles.each((i, el) => {
+		const ballName = pretty($(".title", el).text());
+		if (ballName)
+			stormBallList.push({ name: ballName, companyName: "Storm" });
+	});
+	productTiles.each((i, el) => {
+		const ballUrl = $("a", el).prop("href");
+		stormBallList[i].url = stormURL + ballUrl;
+	});
+
+	// stormBallList.forEach(async (ball) => {
+	//     if (ball.url) {
+	//         const stormBallDetailRawData = await getRawData(ball.url)
+	//         const $ = cheerio.load(stormBallDetailRawData)
+	//         const span = $("p")
+	//         span.each((i, el) => {
+	//             if ($("p", el).text().includes("Release Date")) console.log( $("p", el).text())
+	//         })
+	//     }
+	// })
+	stormBallList.forEach((ball) => ballList.push(ball));
+    getBallDetailsTestFunction(ballList[0])
+};
+
+const getBallDetailsTestFunction = async (ball: BallModel) => {
+	if (ball.url) {
+		const stormBallDetailRawData = await getRawData(ball.url);
+		const $ = cheerio.load(stormBallDetailRawData);
+		const span = $(".product-detail > .row-fluid > .span4");
+        console.log(span.html())
+		// span.each((i, el) => {
+		// 	if ($("p", el).text().includes("Release Date"))
+		// 		console.log($("p", el).text());
+		// });
+	}
 };
 
 getStormBallList();
