6aed7cc5f9d380979dbedd1fe0239ad1420d06db
Scott Hubert <scott@hubertart.com>
2022-08-10 22:03:36 -0600

    got specs, working on covername and covertype and coreType

--- src/hammer/conversions.ts
+++ src/hammer/conversions.ts
@@ -11,56 +11,84 @@ export const getHammerBallDetails = async (
 		const hammerBallDetailRawData = await getRawData(ball.url);
 		const $ = cheerio.load(hammerBallDetailRawData);
 
-		
-        // get description
+		// get description
 		const description = $("p").first().next().text();
 		if (description && description.slice(0, 1) !== "\n")
 			ball.description = description;
-		
+
 		// get image url
-        const imageElement = $(".photo-zoom-link__initial")
-        const imageURLDirty = imageElement.attr("data-src")
-        const width = "720"
-        const imageURLFixedWidth = imageURLDirty?.replace("{width}", width)
-        const imageURLClean = imageURLFixedWidth?.replace("//", "https://")
-        if (imageURLClean) ball.imageUrl = imageURLClean
+		const imageElement = $(".photo-zoom-link__initial");
+		const imageURLDirty = imageElement.attr("data-src");
+		const width = "720";
+		const imageURLFixedWidth = imageURLDirty?.replace("{width}", width);
+		const imageURLClean = imageURLFixedWidth?.replace("//", "https://");
+		if (imageURLClean) ball.imageUrl = imageURLClean;
 
-        const descriptions = $(".product-single__description ul li")
-        descriptions.each((i, el) => {
-            const strongText = $("strong", el).text()
-            const spanText = $("span", el).text()
-            console.log(strongText)
-            if (strongText.match(/part number/gi)) {ball.companyBallId = spanText.trim()}
-            else if (strongText.match(/color/gi)) {ball.color = spanText.trim()}
-            else if (strongText.match(/coverstock/gi)) {ball.coverName = spanText.trim()}
-            else if (strongText.match(/core/gi)) {ball.coreName = spanText.trim()}
-        })
+		const descriptions = $(".product-single__description ul li");
+		descriptions.each((i, el) => {
+			const strongText = $("strong", el).text();
+			const spanText = $("span", el).text();
+			if (strongText.match(/part number/gi)) {
+				ball.companyBallId = spanText.trim();
+			} else if (strongText.match(/color/gi)) {
+				ball.color = spanText.trim();
+			} else if (strongText.match(/coverstock/gi)) {
+				ball.coverName = spanText.trim();
+			} else if (strongText.match(/core type/gi)) {
+				ball.coreType = spanText.trim().match(/asymmetric/gi)
+					? "asymmetrical"
+					: "symmetrical";
+			} else if (strongText.match(/core/gi)) {
+				ball.coreName = spanText.trim();
+			} else if (strongText.match(/cover type/gi)) {
+				switch (spanText.trim()) {
+					case "Pearl Reactive":
+						ball.coverType = "pearl reactive";
+					case "Hybrid Reactive":
+						ball.coverType = "hybrid reactive";
+					case "Solid Reactive":
+						ball.coverType = "solid reactive";
+					case "Pearl Urethane":
+						ball.coverType = "pearl urethane";
+					case "Solid Urethane":
+						ball.coverType = "solid urethane";
+					case "Polyester":
+						ball.coverType = "polyurethane";
+					default:
+						ball.coverType = "other";
+				}
+			} else if (strongText.match(/finish/gi)) {
+				ball.factoryFinish = spanText.trim();
+			}
+		});
 
-		// // get item number
-        // const partNumberEl = $(".product-single__description ul li").first().next().text()
-        // const partNumber = partNumberEl.slice(13).trim()
-        // if (partNumber) ball.companyBallId = partNumber
-
-		// // get release date (n/a)
-
-		// // get color
-        // const colorEl = $(".product-single__description ul li").first().next().next().text()
-        // const color = colorEl.slice(6).trim()
-        
-
-		// setup core/cover details
-
-		// get core name
-
-		// get core type
-
-		// get cover name
-
-		// get cover type
-
-		// get factory finish
-
-		// get specs
+        const specs: BallSpecs[] = []
+		const specsEl = $(".spec-weights li");
+		specsEl.each((i, el) => {
+			const text = $(el).text();
+			const weight = parseInt(text.slice(0, 3));
+			const rgStart = text.indexOf("(");
+			const rgEnd = text.indexOf(")");
+			const rg = parseFloat(text.substring(rgStart + 1, rgEnd));
+			const diffStart = text.indexOf("(", rgEnd);
+			const diffEnd = text.indexOf(")", diffStart);
+			const diff = parseFloat(text.substring(diffStart + 1, diffEnd));
+			let intDiff = null;
+			if (text.match(/asy/gi)) {
+				const asyStart = text.indexOf("(", diffEnd);
+				const asyEnd = text.indexOf(")", asyStart);
+				const asy = text.substring(asyStart + 1, asyEnd);
+				intDiff = parseFloat(asy);
+			}
+            specs.push({
+                weight: weight,
+                rg: rg,
+                diff: diff,
+                intDiff: intDiff
+            })
+		});
+        ball.specs = specs
+		
 	}
 	return ball;
 };
